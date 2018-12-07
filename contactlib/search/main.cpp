288e2b9a98f4e6d2090ce4ee81c370d49d841f5d
zcx <zcx@xenial>
2018-12-06 19:40:08 -0500

    improve search document

--- contactlib/search/main.cpp
+++ contactlib/search/main.cpp
@@ -1,3 +1,13 @@
+const char * docstring="search query database cutoff output\n"
+"  query    - query fingerpint in contactlib-DNN format.\n"
+"  database - contactlib-DNN format structure database.\n"
+"  cutoff   - an int to initialize database. Recommended value: 12\n"
+"  output   - output file name\n"
+"\n"
+"Example:\n"
+"  search 3aa0a.cl ~/.cache/contactlib/contactlib-l4-g0-c2-d7.db 12 output.txt\n"
+;
+
 #include "Library.h"
 #include "Common.h"
 #include "Search.h"
@@ -7,7 +17,11 @@ using namespace std;
 int main(int argc, char **args)
 {
   // parse parameters
-  assert(argc == 5);
+  if (argc != 5)
+  {
+    cerr<<docstring<<endl;
+    return 1;
+  }
   int index = 1;
   string tfn = args[index++];
   string dbfn = args[index++];
@@ -26,4 +40,5 @@ int main(int argc, char **args)
   }
   deleteDB(db);
   cerr << "done!!!" << endl;
+  return 0;
 }
